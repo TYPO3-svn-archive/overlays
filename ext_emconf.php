e38102da6b2103c60406a71810864a2e2f775b6d
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-07-10 08:41:14 +0000

    Released to TER as version 2.1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/overlays/trunk@64335 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "overlays".
 #
-# Auto generated 27-06-2011 22:38
+# Auto generated 10-07-2012 10:41
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,17 +29,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '2.0.1',
+	'version' => '2.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-4.7.99'
+			'typo3' => '4.5.0-4.7.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"4ba4";s:10:"README.txt";s:4:"b729";s:21:"class.tx_overlays.php";s:4:"d1d8";s:16:"ext_autoload.php";s:4:"ad6d";s:12:"ext_icon.gif";s:4:"73e0";s:14:"doc/manual.pdf";s:4:"dadf";s:14:"doc/manual.sxw";s:4:"5e82";}',
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"6aac";s:21:"class.tx_overlays.php";s:4:"192f";s:16:"ext_autoload.php";s:4:"ad6d";s:12:"ext_icon.gif";s:4:"73e0";s:10:"README.txt";s:4:"b729";s:14:"doc/manual.pdf";s:4:"f295";s:14:"doc/manual.sxw";s:4:"622c";}',
 	'suggests' => array(
 	),
 );
